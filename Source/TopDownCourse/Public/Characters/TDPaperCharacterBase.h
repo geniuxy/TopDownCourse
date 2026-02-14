e0218df6819fb6e13f9a6bec8e3ff155655c0daf
geniuxy <2238384910@qq.com>
2026-02-14 16:49:18 +0800

    21

---
+++ Source/TopDownCourse/Public/Characters/TDPaperCharacterBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "TDPaperCharacterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWNCOURSE_API ATDPaperCharacterBase : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
